bbb76f07ce4b793431b63faf973efdb1de0b94bd
young-st511 <asdk511@gmail.com>
2022-09-15 17:19:53 +0900

    chapter 9, 10 진행중

--- 9/my-blog/src/components/ReactDocPage.js
+++ 9/my-blog/src/components/ReactDocPage.js
@@ -1,17 +1,33 @@
-import React from "react";
+import axios from "axios";
+import {React, useEffect, useState} from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 export default function ReactDocPage() {
   // docID: 1, 2, 3....
   const params = useParams();
+  const [doc, setDoc] = useState([]);
   console.log(params);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.docId}`);
+      console.log(res);
+      const result = await res.json();
+
+      return result;
+    }
+    fetchData().then((res) => {
+      setDoc(res);
+    });
+  }, []);
 
   const navigate = useNavigate();
 
   return (
     <>
-      <h5 onClick={() => navigate("/")}>logo</h5>
-      <div>ReactDocPage ##{params.docId}</div>
+      <h1 onClick={() => navigate("/")}>logo</h1>
+      <h3>{doc.title}</h3>
+      <div>{doc.body}</div>
     </>
   );
 }
--- 9/my-blog/src/components/ReactPage.js
+++ 9/my-blog/src/components/ReactPage.js
@@ -1,36 +1,50 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { React, useEffect, useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function ReactPage() {
   // api call -> react에 해당하는 글의 목록을 응답 받음.
+  const [docs, setDocs] = useState([]);
+  // const docs = [
+  //   {
+  //     id: 1,
+  //     title: 'React 공부를 시작하면서',
+  //     date: '05/01/2021',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'CRA 없이 리액트 프로젝트 시작하기',
+  //     date: '12/01/2021',
+  //   },
+  //   {
+  //     id: 3,
+  //     title: '이제는 사용해보자 useMemo & useCallback',
+  //     date: '31/01/2021',
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'React 프로젝트에 Prettier 적용하기',
+  //     date: '15/02/2021',
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'React의 setState() 제대로 사용하기',
+  //     date: '28/02/2021',
+  //   },
+  // ];
 
-  const docs = [
-    {
-      id: 1,
-      title: "React 공부를 시작하면서",
-      date: "05/01/2021",
-    },
-    {
-      id: 2,
-      title: "CRA 없이 리액트 프로젝트 시작하기",
-      date: "12/01/2021",
-    },
-    {
-      id: 3,
-      title: "이제는 사용해보자 useMemo & useCallback",
-      date: "31/01/2021",
-    },
-    {
-      id: 4,
-      title: "React 프로젝트에 Prettier 적용하기",
-      date: "15/02/2021",
-    },
-    {
-      id: 5,
-      title: "React의 setState() 제대로 사용하기",
-      date: "28/02/2021",
-    },
-  ];
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      console.log(res);
+      const result = await res.json();
+
+      return result;
+    }
+
+    fetchData().then((res) => {
+      setDocs(res);
+    });
+  }, []);
 
   return (
     <div>
@@ -38,8 +52,7 @@ export default function ReactPage() {
         <Link
           to={`${doc.id}`}
           key={doc.id}
-          style={{ display: "block", margin: "1rem 0" }}
-        >
+          style={{ display: 'block', margin: '1rem 0' }}>
           {doc.title}
         </Link>
       ))}
